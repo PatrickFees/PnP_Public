0c473bc0c495ed6549ecd36581bfc0a6e3fab0b8
patrickfeess <107756955+PatrickFees@users.noreply.github.com>
2022-11-10 22:27:23 +0100

    NovaVorerstFertig

--- Website/PenAndPaperByPat/src/app/star-map/earth-system-new/locationsInSpace/nova/nova.component.ts
+++ Website/PenAndPaperByPat/src/app/star-map/earth-system-new/locationsInSpace/nova/nova.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nova',
   templateUrl: './nova.component.html',
   styleUrls: ['./nova.component.css']
 })
-export class NovaComponent implements OnInit {
+export class NovaComponent implements OnInit
+{
 
-  constructor() { }
+  constructor(private router: Router) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void
+  {
   }
 
+  public changeBackgroundImage(imageUrl: string)
+  {
+    var container = document.getElementById("container");
+    if (container != null)
+    {
+      container.style.backgroundImage = "url(" + imageUrl + ")";
+    }
+
+  }
+
+  public navigate(page: string)
+  {
+    this.router.navigateByUrl('/' + page);
+  }
+  top: any;
+  left: any;
+  expand = false;
+
+  @HostListener('document:mousemove', ['$event'])
+  onMousemove($event: { pageY: number; pageX: number; })
+  {
+    this.top = ($event.pageY - 10) + "px";
+    this.left = ($event.pageX - 10) + "px";
+  }
 }
+function getElementById(arg0: string)
+{
+  throw new Error('Function not implemented.');
+}
+
