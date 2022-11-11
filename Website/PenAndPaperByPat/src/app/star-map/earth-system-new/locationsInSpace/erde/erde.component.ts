2ff125a54a62664c34df67b1074ed2c675cd6f54
patrickfeess <107756955+PatrickFees@users.noreply.github.com>
2022-11-11 20:05:04 +0100

    neue Webseitensachen

--- Website/PenAndPaperByPat/src/app/star-map/earth-system-new/locationsInSpace/erde/erde.component.ts
+++ Website/PenAndPaperByPat/src/app/star-map/earth-system-new/locationsInSpace/erde/erde.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-erde',
   templateUrl: './erde.component.html',
   styleUrls: ['./erde.component.css']
 })
-export class ErdeComponent implements OnInit {
+export class ErdeComponent implements OnInit
+{
 
-  constructor() { }
+  constructor(private router: Router) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void
+  {
   }
 
+  public changeBackgroundImage(imageUrl: string)
+  {
+    var container = document.getElementById("container");
+    if (container != null)
+    {
+      container.style.backgroundImage = "url(" + imageUrl + ")";
+    }
+
+  }
+
+  public navigate(page: string)
+  {
+    this.router.navigateByUrl('/' + page);
+  }
+  top: any;
+  left: any;
+  expand = false;
+
+  @HostListener('document:mousemove', ['$event'])
+  onMousemove($event: { pageY: number; pageX: number; })
+  {
+    this.top = ($event.pageY - 10) + "px";
+    this.left = ($event.pageX - 10) + "px";
+  }
 }
+
